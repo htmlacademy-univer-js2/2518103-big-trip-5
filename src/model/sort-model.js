d0e39e40928d2c847cc997d572b62a0fd37ba02a
Емельянов Максим <masiko-17@mail.ru>
2025-06-01 15:46:40 +0500

    Рефакторинг

--- src/model/sort-model.js
+++ src/model/sort-model.js
@@ -2,14 +2,14 @@ import Observable from '../framework/observable.js';
 import { SortType } from '../consts.js';
 
 export default class SortModel extends Observable {
-    #currentSortType = SortType.DAY;
+  #currentSortType = SortType.DAY;
 
-    get currentSortType() {
-        return this.#currentSortType;
-    }
+  get currentSortType() {
+    return this.#currentSortType;
+  }
 
-    setSort(updateType, sortType) {
-        this.#currentSortType = sortType;
-        this._notify(updateType, sortType);
-    }
+  setSort(updateType, sortType) {
+    this.#currentSortType = sortType;
+    this._notify(updateType, sortType);
+  }
 }
